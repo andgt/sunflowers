7690e87239bd52d51fd9a74b47a9589522c00cb3
Anatoly <anatolijdegtyar@yandex.ru>
2021-07-13 14:37:57 +0300

    Стилизует форму, добавляет плагины выбора времени и даты

---
+++ source/js/timepicker.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('#datetimepicker').timepicker({
+    timeFormat: 'HH:mm',
+    interval: 30,
+    minTime: '8:30',
+    maxTime: '22:30',
+    startTime: '8:30',
+  });
+});
