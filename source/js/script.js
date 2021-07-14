3675c9eea530a0e6a004d046b66e1033fac4ba27
Anatoly <anatolijdegtyar@yandex.ru>
2021-07-14 15:44:16 +0300

    Добавляет модальное окно

--- source/js/script.js
+++ source/js/script.js
@@ -212,3 +212,93 @@ window.onscroll = function() {
   menuFixed();
   scrollUpButton();
 };
+
+// Модальные окна
+
+let modal = function () {
+  let htmlPage = document.querySelector(".page");
+  let modalsOpen = document.querySelectorAll(".js-button-open");
+  let modalWindows = document.querySelectorAll(".modal");
+  let modalOverlay = document.querySelector(".modal__overlay");
+  let modalFormBack = document.querySelector(".modal__form--backcall");
+  let buttonsClose = document.querySelectorAll(".js-button-close");
+  let modalUsernames = document.querySelectorAll(".modal__form-input--username");
+  let modalBackCall = document.querySelector(".modal");
+  let inputPhone = document.querySelector(".modal__form-input--phone");
+  let username = document.querySelector(".modal__form-input--username");
+  let modalButtons = document.querySelectorAll(".modal__button");
+
+  let modalOpenName;
+  let scrollPosition;
+  let modalName;
+
+  // Автофокус
+
+  let autofocus = function () {
+    for (let modalUsername of modalUsernames) {
+      modalUsername.focus();
+    }
+  };
+
+  // Открытие окна
+
+  for (let modalOpen of modalsOpen) {
+    modalOpen.onclick = function (evt) {
+      evt.preventDefault();
+      scrollPosition = pageYOffset;
+      modalOpenName = this.getAttribute("data-modal-window");
+      modalName = this.getAttribute("data-modal-name");
+      for (let modalWindow of modalWindows) {
+        if (modalWindow.classList.contains(modalOpenName)) {
+          htmlPage.classList.add("page__modal-opened");
+          htmlPage.style.top = -scrollPosition + "px";
+          modalOverlay.classList.add("modal-overlay__open");
+          modalWindow.classList.add("modal__show");
+          autofocus();
+        }
+      }
+    }
+  };
+
+  // Закрытие окна
+
+  let closesWindows = function () {
+    for (let modalWindow of modalWindows) {
+      htmlPage.classList.remove("page__modal-opened");
+      modalWindow.classList.remove("modal__show");
+      modalWindow.classList.remove("modal-error");
+      modalOverlay.classList.remove("modal-overlay__open");
+      htmlPage.style.scrollBehavior = "auto";
+      window.scrollTo(0, scrollPosition);
+      htmlPage.style.scrollBehavior = "";
+      htmlPage.style.top = "";
+    }
+  };
+
+  // Закрытие окна по кнопке
+
+  for (let buttonClose of buttonsClose) {
+    buttonClose.onclick = function (evt) {
+      evt.preventDefault();
+      closesWindows();
+    }
+  };
+
+  // Закрытие окна по esc
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      closesWindows();
+    }
+  });
+
+  // Закрытие по клику вне окна
+
+  htmlPage.addEventListener("click", function (evt) {
+    if (evt.target.classList.contains("modal__overlay")) {
+      closesWindows();
+    }
+  });
+};
+
+modal();
