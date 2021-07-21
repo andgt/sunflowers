493a4e302c8ad611379394036b9335d8880f3e61
anatoliy <anatolijdegtyar@yandex.ru>
2021-07-21 11:49:00 +0300

    Правки

--- source/js/slick-slider.js
+++ source/js/slick-slider.js
@@ -34,37 +34,37 @@ $(document).ready(function() {
 });
 
 
-  $('.slick-slider__3').slick({
-    mobileFirst: true,
-    responsive: [
-      {
-        breakpoint: 320,
-          settings: {
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            speed: 1000,
-          }
-      },
-      {
-        breakpoint: 768,
-          settings: "unslick"
-      },
-    ]
-  });
+$('.slick-slider__3').slick({
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 320,
+        settings: {
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          speed: 1000,
+        }
+    },
+    {
+      breakpoint: 768,
+        settings: "unslick"
+    },
+  ]
+});
 
-  $(window).resize(function() {
-    if (window.innerWidth < 768) {
-      $('.slick-slider__3').not('.slick-initialized').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 1000
-      })
-    } else {
-        $('.slick-slider__3').slick('unslick');
-      }
-  });
+$(window).resize(function() {
+  if (window.innerWidth < 768) {
+    $('.slick-slider__3').not('.slick-initialized').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 1000
+    })
+  } else {
+      $('.slick-slider__3').slick('unslick');
+    }
+});
 
 $(document).ready(function() {
   $('.slick-slider__4').slick({
